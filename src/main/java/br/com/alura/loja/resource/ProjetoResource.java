08f8dbe0ba769561e1646d7b822fceaafae18074
thiagomess <thiagogomes19@hotmail.com>
2018-10-23 22:28:06 -0200

    Usando Gson()

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -14,11 +14,11 @@ public class ProjetoResource {
 	
 	@GET
 	@Path("{id}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String busca(@PathParam("id") long id) {
 		Projeto projeto = new ProjetoDAO().busca(id);
 		
-		return projeto.toXML();
+		return projeto.toJson();
 		
 		
 	}
